492ee1e32188e46cd53462a64825c52986dcd5fd
Diego Silva <diegprocopio98@gmail.com>
2024-07-02 18:04:21 -0300

    feat: update validation check-in

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,8 @@
 import { CheckIn } from '@prisma/client'
 import { CheckInRepository } from '@/repositories/check-ins-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found'
+import dayjs from 'dayjs'
+import { LateCheckInValidateError } from './errors/late-check-in-validate-error'
 
 interface ValidateUseCaseRequest {
   checkInId: string
@@ -19,6 +21,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      'minutes',
+    )
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidateError()
     }
 
     checkIn.validated_at = new Date()
