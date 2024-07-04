cf185d2efe5c53e372715138d46c43ecbcf6181c
Diego Silva <diegprocopio98@gmail.com>
2024-07-04 20:14:53 -0300

    feat(repositories): add repository `check-ins`

--- src/repositories/prisma/prisma-usuarios-repository.ts
+++ src/repositories/prisma/prisma-usuarios-repository.ts
@@ -3,6 +3,12 @@ import { Prisma } from '@prisma/client'
 import { UserRepository } from '../users-repository'
 
 export class PrismaUserRepository implements UserRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({ where: { id } })
+
+    return user
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({ data })
 
